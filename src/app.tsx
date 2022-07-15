cabe6859ea45c621ebc832b4332f726cab9a4597
lv <992526373@qq.com>
2022-07-15 17:04:42 +0800

    css文件单独打包

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,7 @@ const Hello: FC<{ title: string }> = ({ title }) => {
     return (
         <h1>
             <img src={logo} />
-            {title}sss
+            {title}sssd
         </h1>
     );
 };
